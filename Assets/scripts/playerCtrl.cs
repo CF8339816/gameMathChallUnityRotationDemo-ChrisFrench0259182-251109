d226856048e80f3c6b4690103ba41bfc3882532c
CF8339816 <chris_french_3dga@hotmail.com>
2025-11-06 13:50:30 -0400

    beginning code

--- Assets/scripts/playerCtrl.cs
+++ Assets/scripts/playerCtrl.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 public class playerCtrl : MonoBehaviour
 {
+
+    [SerializeField] GameObject TarEne;
+    [SerializeField] GameObject TarOri;
+    [SerializeField] GameObject Player;
+    [SerializeField] Vector3 TarLocEne;
+    [SerializeField] Vector3 TarLocOri;
+    [SerializeField] Vector3 CurrLoc;
+        
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        TarLocEne = TarEne.transform.position;
+        TarLocOri = TarOri.transform.position;
+
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+       transform.LookAt(TarLocEne);
+
+
+
+
     }
 }
