4183c39fb10633e147fded8f4ca210eb765af148
CF8339816 <chris_french_3dga@hotmail.com>
2025-11-06 14:14:34 -0400

    coding movements of targets

---
+++ Assets/scripts/orientCtrl.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
+
+public class smoothDamp : MonoBehaviour
+
+{
+    [SerializeField] GameObject orientLkt;
+    [SerializeField] Vector3 startPosition;
+    [SerializeField] Vector3 targetPosition;
+    [SerializeField] Vector3 endPosition;
+    [SerializeField] float smoothTime = 0.5f;
+    [SerializeField] float stoppingDistance = 0.1f;
+    [SerializeField] Vector3 currentTarget;
+    [SerializeField] Vector3 currentVelocity = Vector3.zero;
+    void Start()
+    {
+        startPosition = orientLkt.transform.position;
+        targetPosition = new Vector3(0, 0, 10);
+        endPosition = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Vector3.Distance(transform.position, currentTarget) < stoppingDistance)
+        {
+            // target switch for return trip check
+            if (currentTarget == targetPosition)
+            {
+                currentTarget = startPosition;
+            }
+            else
+            {
+                currentTarget = targetPosition;
+            }
+        }
+
+        transform.position = Vector3.SmoothDamp(transform.position, currentTarget, ref currentVelocity, smoothTime);
+
+
+    }
+}
